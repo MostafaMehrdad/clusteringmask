28d6c42abdc7333d8c50572ddbab8f6f8674820a
Aidin Tavvafi <at.aidin@gmail.com>
2024-03-26 14:27:04 +0100

    first iteration

--- main.py
+++ main.py
@@ -1,25 +1,104 @@
 import pandas as pd
 from typing import cast
+from dotenv import load_dotenv  # type: ignore
+import os
+import sys
 
-THRESHOLD = 2.5
-ROW = 52
-COL = 55
-
-
-def index_to_tuple(index: int) -> tuple[int, int]:
-    return ((row := index // 52), index - row * 52)
-
-
-def cleanse_file(file: str) -> pd.DataFrame:
-
+def read_file(file: str) -> pd.DataFrame:
     data: pd.DataFrame = pd.read_excel(file)
+    data = cast(pd.DataFrame, data[~data["id"].isna()])
+    data["id"] = data["id"].astype(int)
     data = data.rename(columns={"id": "index", "av(ppm)": "value"})
-    data = cast(pd.DataFrame, data[~data["index"].isna()])
-    data["tuple"] = data["index"].apply(index_to_tuple)
-    data["row"] = data["tuple"].apply(lambda x: x[0]).astype(int)
-    data["col"] = data["tuple"].apply(lambda x: x[1]).astype(int)
-    return cast(pd.DataFrame, data[["row", "col", "value"]])
+    return data
 
+
+def index_to_tuple(index: int, column_quantity: int) -> tuple[int, int]:
+    return (int(row := index // column_quantity), int(index - row * column_quantity))
+
+
+def get_points(df: pd.DataFrame, column_quantity: int) -> dict[tuple[int, int], float]:
+
+    df["tuple"] = df["index"].apply(index_to_tuple, column_quantity=column_quantity)
+    return {
+        x["tuple"]: x["value"]
+        for _, x in cast(pd.DataFrame, df[["tuple", "value"]]).to_dict("index").items()
+    }
+
+
+def find_neighbours(index: tuple[int, int]) -> list[tuple[int, int]]:
+    return [
+        (index[0] + i, index[1] + j)
+        for i in range(-1, 2)
+        for j in range(-1, 2)
+        if i != 0 or j != 0
+    ]
+
+
+def get_cluster(
+    index: tuple[int, int], points: list[tuple[int, int]]
+) -> list[tuple[int, int]]:
+    """
+    get_cluster takes one point and all the eligible points
+    returns the list of all points in the cluster with the input point
+    """
+    cluster: list[tuple[int, int]] = [index]
+    eligible_neighbours = [x for x in find_neighbours(index) if x in points]
+    cluster.extend(eligible_neighbours)
+
+    for neighbour in eligible_neighbours:
+        cluster.extend(get_cluster(neighbour, [x for x in points if x not in cluster]))
+    return cluster
+
+
+def find_clusters(
+    points: dict[tuple[int, int], float], threshold: float
+) -> dict[int, list[tuple[int, int]]]:
+
+    result: dict[int, list[tuple[int, int]]] = {}
+    result[0] = [p for p, v in points.items() if v < threshold]
+
+    eligible_points = {p: v for p, v in points.items() if v >= threshold}
+    remaining_points = list(eligible_points.keys())
+
+    cluster_counter = 1
+    while remaining_points:
+        current_point = remaining_points[0]
+        cluster_points = get_cluster(current_point, remaining_points)
+        remaining_points = [x for x in remaining_points if x not in cluster_points]
+        result[cluster_counter] = cluster_points
+        cluster_counter += 1
+    return result
+
+
+def create_cluster_df(
+    clusters_dict: dict[int, list[tuple[int, int]]], column_quantity: int
+) -> pd.DataFrame:
+    index_to_cluster = {
+        z[0] * column_quantity + z[1]: x for x, y in clusters_dict.items() for z in y
+    }
+    return pd.DataFrame.from_dict(index_to_cluster, orient="index").rename(
+        columns={0: "cluster"}
+    )
+
+def main(file_name: str) -> None:
+    load_dotenv()
+    threshold = float(os.environ.get("THRESHOLD", 0))
+    col = int(os.environ.get("COL", 0))
+    if not threshold or not col:
+        raise ValueError("please populate .env file with proper values.")
+
+    df = read_file(file_name)
+    points = get_points(df.copy(deep=True), col)
+    clusters = find_clusters(points, threshold)
+    cluster_df = create_cluster_df(clusters, col)
+    df.set_index("index", inplace=True)
+    result_df = df.join(cluster_df)
+    result_df.reset_index(inplace=True)
+
+    if not os.path.isdir("output"):
+        os.mkdir("output")
+    result_df.rename(columns={"index": "id", "value": "av(ppm)"}, inplace=True)
+    result_df.to_excel(f"output/{file_name.split("/")[-1]}", index=False)
 
 if __name__ == "__main__":
-    df = cleanse_file("data/uBB-data.xlsx")
+    main(sys.argv[1])
